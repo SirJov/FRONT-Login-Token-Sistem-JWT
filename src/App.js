b8807de936be62cb5d0571b5b96366d10512b12d
Heric_ <herico_campos@hotmail.com>
2023-05-04 22:33:40 -0300

    Adicionando corpo dos componentes

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import Header from "./Components/Header/Header";
 import BodyAplication from "./Components/Body/BodyAplication";
 import Footer from "./Components/Footer/Footer";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
---
+++ src/Pages/Private/Private.js
@@ -0,0 +1,11 @@
+
+import React from "react";
+import "./Private.css";
+
+export default function Private() {
+  return (
+    <section>
+      <div></div>
+    </section>
+  );
+}
