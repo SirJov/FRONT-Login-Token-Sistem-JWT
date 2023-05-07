e7b6f33152cdb4ae8f5ec8726ab265f7d0cb6972
Heric_ <herico_campos@hotmail.com>
2023-05-07 11:31:48 -0300

    Add tela cadastro

--- src/Components/Body/LoginBody/LoginBody.jsx
+++ src/Components/Body/LoginBody/LoginBody.jsx
@@ -1,16 +1,58 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+
 import "./LoginBody.css";
 
 export default function LoguinBody() {
+  const [User, setUser] = useState();
+  const [Password, setPassword] = useState();
+
+  const [dataLogin, setDataLogin] = useState();
+
+  const UserLogin = (event) => {
+    setUser(event.target.value);
+    setador();
+  };
+  const PasswordLogin = (event) => {
+    setPassword(event.target.value);
+    setador();
+  };
+
+  function setador() {
+    setDataLogin({
+      email: User,
+      password: Password,
+    });
+  }
+
+  const ReqLoguin = (event) => {
+    event.preventDefault();
+
+    console.log(dataLogin);
+  };
+
   return (
     <div className="LoginBody">
       <form>
-        <input type="text" placeholder="Usuario ou Email"></input>
-        <input type="text" placeholder="Sua Senha"></input>
-        <button>Entrar</button>
+        <input
+          type="text"
+          placeholder="Email"
+          required
+          onChange={UserLogin}
+        ></input>
+        <input
+          type="text"
+          placeholder="Sua Senha"
+          required
+          onChange={PasswordLogin}
+        ></input>
+        <button onClick={ReqLoguin}>Entrar</button>
       </form>
       <div>
         <p>Nao tem cadastro ?</p>
-        <button>Cadastrar</button>
+        <Link to="/Register">
+          <button>Cadastro</button>
+        </Link>
       </div>
     </div>
   );
--- src/Components/Body/RegisterBody/RegisterBody.js
+++ src/Components/Body/RegisterBody/RegisterBody.js
@@ -1,12 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import "./RegisterBody.css";
 
 export default function RegisterBody() {
+  const [User, setUser] = useState();
+  const [Email, setEmail] = useState();
+  const [Password, setPassword] = useState();
+
+  const [dataLogin, setDataLogin] = useState();
+
   return (
     <div className="RegisterBody">
-      <p>SSSSSSSSSSSSSSSS</p>
-      <p>SSSSSSSSSSSSSSSS</p>
-      <p>SSSSSSSSSSSSSSSS</p>
+      <form>
+        <input type="text" placeholder="Usuario" required onChange={""}></input>
+        <input type="text" placeholder="Email" required onChange={""}></input>
+        <input
+          type="text"
+          placeholder="Sua Senha"
+          required
+          onChange={""}
+        ></input>
+        <button onClick={""}>Cadastrar</button>
+      </form>
     </div>
   );
 }
