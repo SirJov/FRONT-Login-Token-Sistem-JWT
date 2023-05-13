64572f86fd90b622fbc8cb198cea2d2dda4771cc
Heric_ <herico_campos@hotmail.com>
2023-05-13 10:51:00 -0300

    Add imgs Profiles

--- src/Components/Header/DetailDivHeader/DetailDivHeader.js
+++ src/Components/Header/DetailDivHeader/DetailDivHeader.js
@@ -4,7 +4,6 @@ import "./DetailDivHeader.css";
 export default function DetailDivHeader() {
   return (
     <div>
-      <div className="boxEfect1"></div>
       <div className="boxEfect2"></div>
       <div className="boxEfect3"></div>
     </div>
