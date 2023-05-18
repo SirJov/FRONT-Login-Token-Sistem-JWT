44e93017bd4e841f481a5d73fd0ee11b84a79053
Heric_ <herico_campos@hotmail.com>
2023-05-17 22:08:56 -0300

    Add btn reload Feed functionalit

--- src/Components/Body/PrivateRoutes/FeedBody/SectionPostFeed/SectionPostFeed.jsx
+++ src/Components/Body/PrivateRoutes/FeedBody/SectionPostFeed/SectionPostFeed.jsx
@@ -1,6 +1,39 @@
+import axios from "axios";
 import "./SectionPostFeed.css";
+import ContextToken from "../../../../../Context/ContextToken";
+import ContextFeed from "../../../../../Context/ContextFeed";
+import { useContext } from "react";
+import { useNavigate } from "react-router-dom";
 
 export default function SectionPostFeed() {
+  const [tokenUser, SetTokenUser] = useContext(ContextToken);
+  const [feedData, SetFeedData] = useContext(ContextFeed);
+  const navigate = useNavigate();
+
+  const GetFeed = async () => {
+    const token = tokenUser;
+    const config = {
+      headers: { Authorization: `Bearer ${token}` },
+    };
+
+    const data = await axios
+      .get("https://api-login-token-sistem-jwt.vercel.app/FeedGet", config)
+      .then((response) => {
+        return SetFeedData(response.data[1].obj);
+      })
+      .catch((error) => {
+        const ErrorToken = error.response.data[0].error.name;
+
+        if (ErrorToken == "JsonWebTokenError" || "TokenExpiredError") {
+          localStorage.removeItem("userData");
+          localStorage.removeItem("token");
+          SetTokenUser(undefined);
+          return navigate("/");
+        }
+      });
+    return data;
+  };
+
   return (
     <div className="SectionPostFeed">
       <div className="FuncionalitComponentsPostFeed">
@@ -11,7 +44,7 @@ export default function SectionPostFeed() {
           <button>
             <a href="#Topo">⇧</a>
           </button>
-          <button>↻</button>
+          <button onClick={GetFeed}>↻</button>
         </div>
       </div>
     </div>
